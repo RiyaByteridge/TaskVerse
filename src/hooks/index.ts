47febe19bba5719de421a17f8acc249d3d4e1b1a
unknown <riyak@byteridge.com>
2023-10-27 09:26:57 +0530

    Index

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,5 @@
 import useAxiosGetHook from "./useAxiosGet.hook";
 import useAxiosPostHook from "./useAxiosPost.hook";
-export { useAxiosGetHook ,useAxiosPostHook};
+import useAxiosDeleteHook from "./useAxiosDelete.hook";
+import useAxiosUpdateHook from "./useAxiosUpdate.hook";
+export { useAxiosGetHook ,useAxiosPostHook ,useAxiosDeleteHook,useAxiosUpdateHook};
