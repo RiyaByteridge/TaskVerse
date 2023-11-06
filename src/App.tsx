5e2f66950915ba0606c7f5e6060f154870c3ff74
unknown <riyak@byteridge.com>
2023-11-06 11:14:59 +0530

    loginscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
+// import { LoginScreen } from "./components/LogiScreen";
+import {useAxiosHook } from './hooks/index'
+import { API_ENDPOINT } from "./services/apiEndpoints";
+import { LoginScreen } from "./components/LoginScreen";
+
+
 function App() {
-  
-  return <h2>Hello There</h2>;
+  useAxiosHook({ url: API_ENDPOINT.products.getAllProducts, method: "GET" });
+  return (
+    <>
+    <LoginScreen />
+
+    </>
+  );
 }
 
 export default App;
+
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,3 @@
-import useAxiosGetHook from "./useAxiosGet.hook";
-import useAxiosPostHook from "./useAxiosPost.hook";
-import useAxiosDeleteHook from "./useAxiosDelete.hook";
-import useAxiosUpdateHook from "./useAxiosUpdate.hook";
-export { useAxiosGetHook ,useAxiosPostHook ,useAxiosDeleteHook,useAxiosUpdateHook};
+import useAxiosHook from "./useAxios.hook";
+
+export { useAxiosHook};
