1e759fb68294b04f2d9f423ef52bc61ee7ff66e5
Shreyash Shrivastava <shreyashs@byteridge.com>
2023-11-06 16:25:42 +0530

    Create Login Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import { useAxiosHook } from "./hooks";
-import { API_ENDPOINT } from "./services/apiEndpoints";
-function App() {
+import { Login } from "./components";
 
-  useAxiosHook({url:API_ENDPOINT.products.getAllProducts,method:"GET"})
-  return (<></>);
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
 }
 
 export default App;
